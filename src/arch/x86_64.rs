392675c1fd0797be702dc3dab21f0232b10c871c
Lokathor <zefria@gmail.com>
2019-07-06 11:14:04 -0600

    start of x86 work

--- src/arch/x86_64.rs
+++ src/arch/x86_64.rs
@@ -1,9 +1,20 @@
 //! Intrinsics for the [x86_64](https://en.wikipedia.org/wiki/X86-64) processor family.
 
+use core::arch::x86_64::*;
+
 /// As [_rdtsc](core::arch::x86_64::_rdtsc), just marked safe since it's always safe.
 #[inline]
 pub fn rdtsc() -> u64 {
   unsafe {
-    core::arch::x86_64::_rdtsc()
+    _rdtsc()
   }
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct m128(pub __m128);
+
+#[derive(Debug, Clone, Copy)]
+pub struct m128i(pub __m128i);
+
+#[derive(Debug, Clone, Copy)]
+pub struct m128d(pub __m128d);
