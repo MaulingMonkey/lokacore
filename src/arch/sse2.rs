747f4d21734ec82453fe3b8e9716096aa8e2b7a6
Lokathor <zefria@gmail.com>
2019-07-16 14:05:49 -0600

    stupid huge formatting impls

--- src/arch/sse2.rs
+++ src/arch/sse2.rs
@@ -33,7 +33,7 @@ impl core::fmt::Debug for m128i {
   ///
   /// * Use `width` to specify the lane count you want (default 1).
   /// * Use `alternate` format specifier to give `uX` instead of `iX` output.
-  /// 
+  ///
   /// Eg, for 4 lanes of `u32`:
   /// ```txt
   /// format!("{:#4?}", m)
@@ -91,6 +91,385 @@ impl core::fmt::Debug for m128i {
         } else {
           let a: i128 = cast(self.0);
           write!(f, "m128i({:?})", a)
+        }
+      }
+    }
+  }
+}
+
+impl core::fmt::Display for m128i {
+  /// Display formats in offset order.
+  ///
+  /// * Use `width` to specify the lane count you want (default 1).
+  /// * Use `alternate` format specifier to give `uX` instead of `iX` output.
+  ///
+  /// Eg, for 4 lanes of `u32`:
+  /// ```txt
+  /// format!("{:#4?}", m)
+  /// ```
+  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+    match f.width() {
+      Some(2) => {
+        if f.alternate() {
+          let a: [u64; 2] = cast(self.0);
+          write!(f, "m128i({}, {})", a[0], a[1])
+        } else {
+          let a: [i64; 2] = cast(self.0);
+          write!(f, "m128i({}, {})", a[0], a[1])
+        }
+      }
+      Some(4) => {
+        if f.alternate() {
+          let a: [u32; 4] = cast(self.0);
+          write!(f, "m128i({}, {}, {}, {})", a[0], a[1], a[2], a[3])
+        } else {
+          let a: [i32; 4] = cast(self.0);
+          write!(f, "m128i({}, {}, {}, {})", a[0], a[1], a[2], a[3])
+        }
+      }
+      Some(8) => {
+        if f.alternate() {
+          let a: [u16; 8] = cast(self.0);
+          write!(
+            f,
+            "m128i({}, {}, {}, {}, {}, {}, {}, {})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        } else {
+          let a: [i16; 8] = cast(self.0);
+          write!(
+            f,
+            "m128i({}, {}, {}, {}, {}, {}, {}, {})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        }
+      }
+      Some(16) => {
+        if f.alternate() {
+          let a: [u8; 16] = cast(self.0);
+          write!(
+            f,
+            "m128i({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})",
+            a[0],
+            a[1],
+            a[2],
+            a[3],
+            a[4],
+            a[5],
+            a[6],
+            a[7],
+            a[8],
+            a[9],
+            a[10],
+            a[11],
+            a[12],
+            a[13],
+            a[14],
+            a[15]
+          )
+        } else {
+          let a: [i8; 16] = cast(self.0);
+          write!(
+            f,
+            "m128i({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})",
+            a[0],
+            a[1],
+            a[2],
+            a[3],
+            a[4],
+            a[5],
+            a[6],
+            a[7],
+            a[8],
+            a[9],
+            a[10],
+            a[11],
+            a[12],
+            a[13],
+            a[14],
+            a[15]
+          )
+        }
+      }
+      _ => {
+        if f.alternate() {
+          let a: u128 = cast(self.0);
+          write!(f, "m128i({})", a)
+        } else {
+          let a: i128 = cast(self.0);
+          write!(f, "m128i({})", a)
+        }
+      }
+    }
+  }
+}
+
+impl core::fmt::Binary for m128i {
+  /// Binary formats in offset order.
+  ///
+  /// * Use `width` to specify the lane count you want (default 1).
+  /// * Use `alternate` format specifier to give leading `0b`.
+  ///
+  /// Eg, for 4 lanes and leading `0b`:
+  /// ```txt
+  /// format!("{:#4b}", m)
+  /// ```
+  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+    match f.width() {
+      Some(2) => {
+        let a: [u64; 2] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#b}, {:#b})", a[0], a[1])
+        } else {
+          write!(f, "m128i({:b}, {:b})", a[0], a[1])
+        }
+      }
+      Some(4) => {
+        let a: [u32; 4] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#b}, {:#b}, {:#b}, {:#b})",
+            a[0], a[1], a[2], a[3]
+          )
+        } else {
+          write!(f, "m128i({:b}, {:b}, {:b}, {:b})", a[0], a[1], a[2], a[3])
+        }
+      }
+      Some(8) => {
+        let a: [u16; 8] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        } else {
+          write!(
+            f,
+            "m128i({:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        }
+      }
+      Some(16) => {
+        let a: [u8; 16] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        } else {
+          write!(f, "m128i({:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        }
+      }
+      _ => {
+        let a: u128 = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#b})", a)
+        } else {
+          write!(f, "m128i({:b})", a)
+        }
+      }
+    }
+  }
+}
+
+impl core::fmt::LowerHex for m128i {
+  /// LowerHex formats in offset order.
+  ///
+  /// * Use `width` to specify the lane count you want (default 1).
+  /// * Use `alternate` format specifier to give leading `0x`.
+  ///
+  /// Eg, for 4 lanes and leading `0x`:
+  /// ```txt
+  /// format!("{:#4x}", m)
+  /// ```
+  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+    match f.width() {
+      Some(2) => {
+        let a: [u64; 2] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#x}, {:#x})", a[0], a[1])
+        } else {
+          write!(f, "m128i({:x}, {:x})", a[0], a[1])
+        }
+      }
+      Some(4) => {
+        let a: [u32; 4] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#x}, {:#x}, {:#x}, {:#x})",
+            a[0], a[1], a[2], a[3]
+          )
+        } else {
+          write!(f, "m128i({:x}, {:x}, {:x}, {:x})", a[0], a[1], a[2], a[3])
+        }
+      }
+      Some(8) => {
+        let a: [u16; 8] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        } else {
+          write!(
+            f,
+            "m128i({:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        }
+      }
+      Some(16) => {
+        let a: [u8; 16] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        } else {
+          write!(f, "m128i({:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        }
+      }
+      _ => {
+        let a: u128 = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#x})", a)
+        } else {
+          write!(f, "m128i({:x})", a)
+        }
+      }
+    }
+  }
+}
+
+impl core::fmt::Octal for m128i {
+  /// Octal formats in offset order.
+  ///
+  /// * Use `width` to specify the lane count you want (default 1).
+  /// * Use `alternate` format specifier to give leading `0o`.
+  ///
+  /// Eg, for 4 lanes and leading `0o`:
+  /// ```txt
+  /// format!("{:#4o}", m)
+  /// ```
+  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+    match f.width() {
+      Some(2) => {
+        let a: [u64; 2] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#o}, {:#o})", a[0], a[1])
+        } else {
+          write!(f, "m128i({:o}, {:o})", a[0], a[1])
+        }
+      }
+      Some(4) => {
+        let a: [u32; 4] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#o}, {:#o}, {:#o}, {:#o})",
+            a[0], a[1], a[2], a[3]
+          )
+        } else {
+          write!(f, "m128i({:o}, {:o}, {:o}, {:o})", a[0], a[1], a[2], a[3])
+        }
+      }
+      Some(8) => {
+        let a: [u16; 8] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        } else {
+          write!(
+            f,
+            "m128i({:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        }
+      }
+      Some(16) => {
+        let a: [u8; 16] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        } else {
+          write!(f, "m128i({:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        }
+      }
+      _ => {
+        let a: u128 = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#o})", a)
+        } else {
+          write!(f, "m128i({:o})", a)
+        }
+      }
+    }
+  }
+}
+
+impl core::fmt::UpperHex for m128i {
+  /// UpperHex formats in offset order.
+  ///
+  /// * Use `width` to specify the lane count you want (default 1).
+  /// * Use `alternate` format specifier to give leading `0x`.
+  ///
+  /// Eg, for 4 lanes and leading `0x`:
+  /// ```txt
+  /// format!("{:#4X}", m)
+  /// ```
+  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+    match f.width() {
+      Some(2) => {
+        let a: [u64; 2] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#X}, {:#X})", a[0], a[1])
+        } else {
+          write!(f, "m128i({:X}, {:X})", a[0], a[1])
+        }
+      }
+      Some(4) => {
+        let a: [u32; 4] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#X}, {:#X}, {:#X}, {:#X})",
+            a[0], a[1], a[2], a[3]
+          )
+        } else {
+          write!(f, "m128i({:X}, {:X}, {:X}, {:X})", a[0], a[1], a[2], a[3])
+        }
+      }
+      Some(8) => {
+        let a: [u16; 8] = cast(self.0);
+        if f.alternate() {
+          write!(
+            f,
+            "m128i({:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        } else {
+          write!(
+            f,
+            "m128i({:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X})",
+            a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+          )
+        }
+      }
+      Some(16) => {
+        let a: [u8; 16] = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        } else {
+          write!(f, "m128i({:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X})", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15])
+        }
+      }
+      _ => {
+        let a: u128 = cast(self.0);
+        if f.alternate() {
+          write!(f, "m128i({:#X})", a)
+        } else {
+          write!(f, "m128i({:X})", a)
         }
       }
     }
--- tests/sse2.rs
+++ tests/sse2.rs
@@ -47,3 +47,275 @@ fn m128i_debug() {
     )
   );
 }
+
+#[test]
+fn m128i_display() {
+  let m: m128i = cast(core::u128::MAX);
+  let max = core::u128::MAX;
+  assert_eq!(&format!("{:}", m), "m128i(-1)");
+  assert_eq!(&format!("{:#}", m), &format!("m128i({})", max));
+  assert_eq!(&format!("{:1}", m), "m128i(-1)");
+  assert_eq!(&format!("{:#1}", m), &format!("m128i({})", max));
+  let max = core::u64::MAX;
+  assert_eq!(&format!("{:2}", m), "m128i(-1, -1)");
+  assert_eq!(&format!("{:#2}", m), &format!("m128i({}, {})", max, max));
+  let max = core::u32::MAX;
+  assert_eq!(&format!("{:4}", m), "m128i(-1, -1, -1, -1)");
+  assert_eq!(
+    &format!("{:#4}", m),
+    &format!("m128i({}, {}, {}, {})", max, max, max, max)
+  );
+  let max = core::u16::MAX;
+  assert_eq!(&format!("{:8}", m), "m128i(-1, -1, -1, -1, -1, -1, -1, -1)");
+  assert_eq!(
+    &format!("{:#8}", m),
+    &format!(
+      "m128i({}, {}, {}, {}, {}, {}, {}, {})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  let max = core::u8::MAX;
+  assert_eq!(
+    &format!("{:16}", m),
+    "m128i(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)"
+  );
+  assert_eq!(
+    &format!("{:#16}", m),
+    &format!(
+      "m128i({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+}
+
+#[test]
+fn m128i_binary() {
+  let m: m128i = cast(core::u128::MAX);
+  let max = core::u128::MAX;
+  assert_eq!(&format!("{:b}", m), &format!("m128i({:b})", max));
+  assert_eq!(&format!("{:#b}", m), &format!("m128i({:#b})", max));
+  assert_eq!(&format!("{:1b}", m), &format!("m128i({:b})", max));
+  assert_eq!(&format!("{:#1b}", m), &format!("m128i({:#b})", max));
+  let max = core::u64::MAX;
+  assert_eq!(
+    &format!("{:2b}", m),
+    &format!("m128i({:b}, {:b})", max, max)
+  );
+  assert_eq!(
+    &format!("{:#2b}", m),
+    &format!("m128i({:#b}, {:#b})", max, max)
+  );
+  let max = core::u32::MAX;
+  assert_eq!(
+    &format!("{:4b}", m),
+    &format!("m128i({:b}, {:b}, {:b}, {:b})", max, max, max, max)
+  );
+  assert_eq!(
+    &format!("{:#4b}", m),
+    &format!("m128i({:#b}, {:#b}, {:#b}, {:#b})", max, max, max, max)
+  );
+  let max = core::u16::MAX;
+  assert_eq!(
+    &format!("{:8b}", m),
+    &format!(
+      "m128i({:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#8b}", m),
+    &format!(
+      "m128i({:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  let max = core::u8::MAX;
+  assert_eq!(
+    &format!("{:16b}", m),
+    &format!(
+      "m128i({:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b}, {:b})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#16b}", m),
+    &format!(
+      "m128i({:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b}, {:#b})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+}
+
+#[test]
+fn m128i_lower_hex() {
+  let m: m128i = cast(core::u128::MAX);
+  let max = core::u128::MAX;
+  assert_eq!(&format!("{:x}", m), &format!("m128i({:x})", max));
+  assert_eq!(&format!("{:#x}", m), &format!("m128i({:#x})", max));
+  assert_eq!(&format!("{:1x}", m), &format!("m128i({:x})", max));
+  assert_eq!(&format!("{:#1x}", m), &format!("m128i({:#x})", max));
+  let max = core::u64::MAX;
+  assert_eq!(
+    &format!("{:2x}", m),
+    &format!("m128i({:x}, {:x})", max, max)
+  );
+  assert_eq!(
+    &format!("{:#2x}", m),
+    &format!("m128i({:#x}, {:#x})", max, max)
+  );
+  let max = core::u32::MAX;
+  assert_eq!(
+    &format!("{:4x}", m),
+    &format!("m128i({:x}, {:x}, {:x}, {:x})", max, max, max, max)
+  );
+  assert_eq!(
+    &format!("{:#4x}", m),
+    &format!("m128i({:#x}, {:#x}, {:#x}, {:#x})", max, max, max, max)
+  );
+  let max = core::u16::MAX;
+  assert_eq!(
+    &format!("{:8x}", m),
+    &format!(
+      "m128i({:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#8x}", m),
+    &format!(
+      "m128i({:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  let max = core::u8::MAX;
+  assert_eq!(
+    &format!("{:16x}", m),
+    &format!(
+      "m128i({:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x}, {:x})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#16x}", m),
+    &format!(
+      "m128i({:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x}, {:#x})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+}
+
+#[test]
+fn m128i_octal() {
+  let m: m128i = cast(core::u128::MAX);
+  let max = core::u128::MAX;
+  assert_eq!(&format!("{:o}", m), &format!("m128i({:o})", max));
+  assert_eq!(&format!("{:#o}", m), &format!("m128i({:#o})", max));
+  assert_eq!(&format!("{:1o}", m), &format!("m128i({:o})", max));
+  assert_eq!(&format!("{:#1o}", m), &format!("m128i({:#o})", max));
+  let max = core::u64::MAX;
+  assert_eq!(
+    &format!("{:2o}", m),
+    &format!("m128i({:o}, {:o})", max, max)
+  );
+  assert_eq!(
+    &format!("{:#2o}", m),
+    &format!("m128i({:#o}, {:#o})", max, max)
+  );
+  let max = core::u32::MAX;
+  assert_eq!(
+    &format!("{:4o}", m),
+    &format!("m128i({:o}, {:o}, {:o}, {:o})", max, max, max, max)
+  );
+  assert_eq!(
+    &format!("{:#4o}", m),
+    &format!("m128i({:#o}, {:#o}, {:#o}, {:#o})", max, max, max, max)
+  );
+  let max = core::u16::MAX;
+  assert_eq!(
+    &format!("{:8o}", m),
+    &format!(
+      "m128i({:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#8o}", m),
+    &format!(
+      "m128i({:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  let max = core::u8::MAX;
+  assert_eq!(
+    &format!("{:16o}", m),
+    &format!(
+      "m128i({:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o}, {:o})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#16o}", m),
+    &format!(
+      "m128i({:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o}, {:#o})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+}
+
+#[test]
+fn m128i_upper_hex() {
+  let m: m128i = cast(core::u128::MAX);
+  let max = core::u128::MAX;
+  assert_eq!(&format!("{:X}", m), &format!("m128i({:X})", max));
+  assert_eq!(&format!("{:#X}", m), &format!("m128i({:#X})", max));
+  assert_eq!(&format!("{:1X}", m), &format!("m128i({:X})", max));
+  assert_eq!(&format!("{:#1X}", m), &format!("m128i({:#X})", max));
+  let max = core::u64::MAX;
+  assert_eq!(
+    &format!("{:2X}", m),
+    &format!("m128i({:X}, {:X})", max, max)
+  );
+  assert_eq!(
+    &format!("{:#2X}", m),
+    &format!("m128i({:#X}, {:#X})", max, max)
+  );
+  let max = core::u32::MAX;
+  assert_eq!(
+    &format!("{:4X}", m),
+    &format!("m128i({:X}, {:X}, {:X}, {:X})", max, max, max, max)
+  );
+  assert_eq!(
+    &format!("{:#4X}", m),
+    &format!("m128i({:#X}, {:#X}, {:#X}, {:#X})", max, max, max, max)
+  );
+  let max = core::u16::MAX;
+  assert_eq!(
+    &format!("{:8X}", m),
+    &format!(
+      "m128i({:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#8X}", m),
+    &format!(
+      "m128i({:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X})",
+      max, max, max, max, max, max, max, max
+    )
+  );
+  let max = core::u8::MAX;
+  assert_eq!(
+    &format!("{:16X}", m),
+    &format!(
+      "m128i({:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X}, {:X})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+  assert_eq!(
+    &format!("{:#16X}", m),
+    &format!(
+      "m128i({:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X}, {:#X})",
+      max, max, max, max, max, max, max, max, max, max, max, max, max, max, max, max
+    )
+  );
+}
