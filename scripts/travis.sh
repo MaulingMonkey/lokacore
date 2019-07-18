64c44d05323fb2a10ef3639b052dc9dc532093de
MaulingMonkey <git@maulingmonkey.com>
2019-07-18 15:23:08 -0700

    travis: Build android, ios, wasm, and linux release builds.

---
+++ scripts/travis.sh
@@ -0,0 +1,34 @@
+set -e
+
+rustup component add clippy
+cargo clippy
+
+if [[ "$TARGET" != "" ]]; then rustup target install $TARGET; fi
+
+if [[ "$TARGET" == "wasm32-"* && "$TARGET" != "wasm32-wasi" ]]; then
+  cargo-web --version || cargo install cargo-web
+  cargo web build $FLAGS --target=$TARGET
+  cargo web test  $FLAGS --target=$TARGET
+
+elif [[ "$TARGET" == *"-linux-android"* ]]; then
+  export PATH=/usr/local/android-sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+  pushd linux-android
+    cargo build --target=$TARGET
+    # Don't test, can't run android emulators successfully on travis currently
+  popd
+
+elif [[ "$TARGET" == *"-apple-ios" || "$TARGET" == "wasm32-wasi" ]]; then
+  cargo build --target=$TARGET $FLAGS
+  # Don't test
+  #   iOS simulator setup/teardown is complicated
+  #   cargo-web doesn't support wasm32-wasi yet, nor can wasm-pack test specify a target
+
+elif [[ "$TARGET" != "" ]]; then
+  cargo build --target=$TARGET $FLAGS
+  cargo test  --target=$TARGET $FLAGS
+
+else
+  cargo build $FLAGS
+  cargo test  $FLAGS
+
+fi
